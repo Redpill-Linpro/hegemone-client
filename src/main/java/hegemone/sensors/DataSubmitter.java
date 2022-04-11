21da1f3b69b6b92d2335893bf7b7d192de50a02b
Billy J. Beltran <billy@redpill-linpro.com>
2022-04-11 05:43:54 +0200

    Add QuestDBConsumer

--- src/main/java/hegemone/sensors/DataSubmitter.java
+++ src/main/java/hegemone/sensors/DataSubmitter.java
@@ -1,16 +1,21 @@
 package hegemone.sensors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 
 public class DataSubmitter {
+    private static final Logger logger = LoggerFactory.getLogger("hegemone.sensors.datasubmitter");
     static List<DataConsumer> consumerList = new ArrayList<>();
     /**
      * Submit data to some kind of data consumer
      */
     public static void submit(String data) {
+        logger.debug("---- submitting data -----\n" + data + "\n----- end data submission frame ------");
         consumerList.forEach(c -> c.accept(data));
     }
 
--- src/main/java/hegemone/sensors/QuestDBConsumer.java
+++ src/main/java/hegemone/sensors/QuestDBConsumer.java
@@ -2,45 +2,56 @@ package hegemone.sensors;
 
 import com.github.wnameless.json.flattener.JsonFlattener;
 import com.google.gson.Gson;
+import io.questdb.cutlass.line.AbstractLineSender;
+import io.questdb.cutlass.line.LineTcpSender;
 import io.questdb.cutlass.line.LineUdpSender;
 import io.questdb.network.Net;
+import io.questdb.std.Os;
 
 import java.util.Map;
 
 public class QuestDBConsumer implements DataConsumer {
-    private LineUdpSender sender;
-    private String IPv4Address;
+
     private String tableName = "hegemone_sensors";
+    private String IPv4Address;
+    private int port;
+
     public QuestDBConsumer(String IPv4Address, int port) {
         this.IPv4Address = IPv4Address;
-        sender = new LineUdpSender(0,
-                Net.parseIPv4(IPv4Address),
-                port, 110, 2);
+        this.port = port;
     }
+
     @Override
     public void accept(String data) {
         var gs = new Gson();
-        var flattenedData = JsonFlattener.flatten(data);
-        Map map = (Map) gs.fromJson(flattenedData, Object.class);
+        var flattenedData = new JsonFlattener(data).withSeparator('/').flatten();
+        var cleaned = flattenedData.replaceAll("/", "_");
+        cleaned = cleaned.replaceAll("\\[(\\d)\\]", "_$1");
+        Map map = (Map) gs.fromJson(cleaned, Object.class);
         var entries = map.entrySet();
         System.out.println(entries);
-//        var metric =   sender.metric(tableName)
-//                .tag("by", "hegemone");
-//        for(Map.Entry entry : entries) {
-//            var column = entry.getKey().toString();
-//            var entry = entry.getValue();
-//            switch(entry) {
-//                case Integer i -> metric.field(column, i);
-//                case Double d -> metric.field(column, d);
-//                case String s -> metric.field(column, s);
-//                case Long l -> metric.field(column, l);
-//                case default -> metric.field(column, entry.toString());
-//            }
-//            metric.flush();
-//        }
+        try (LineTcpSender sender = new LineTcpSender(Net.parseIPv4(IPv4Address), port, 256*1024)) {
+            var metric = sender.metric(tableName)
+                    .tag("by", "hegemone");
+            entries.forEach(e -> {
+                var entry = (Map.Entry<?, ?>) e;
+                var column = entry.getKey().toString();
+                var value = entry.getValue();
+                switch (value) {
+                    case Integer i -> metric.field(column, i);
+                    case Double d -> metric.field(column, d);
+                    case String s -> metric.field(column, s);
+                    case Long l -> metric.field(column, l);
+                    case default -> metric.field(column, value.toString());
+                }
+            });
+            System.out.println("Debug metric\n" + metric.toString());
+            metric.$(Os.currentTimeMicros() * 1000);
+            metric.flush();
+        }
     }
 
-    public void setTableName(String tableName) {
-        this.tableName = tableName;
+        public void setTableName (String tableName){
+            this.tableName = tableName;
+        }
     }
-}
